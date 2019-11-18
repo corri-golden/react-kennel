af344ef3c8a958ec78e535a84405cc55d80cff1d
Corri Golden <corri.golden@gmail.com>
2019-11-18 15:13:22 -0600

    chapter 1 and 2

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -6,30 +6,19 @@ import AnimalCard from './animal/AnimalCard'
 import EmployeeCard from './employee/EmployeeCard'
 import LocationCard from './location/LocationCard'
 import OwnerCard from './owner/OwnerCard'
+import NavBar from './nav/NavBar'
+import ApplicationViews from './ApplicationViews'
 
 
 class Kennel extends Component {
   render() {
     return (
-      <div>
-        < div>
-          <h2>Student Kennels<br />
-            <small>Loving care when you're not there.</small>
-          </h2>
-          <address>
-            Visit Us at the Nashville North Location
-                    <br />500 Puppy Way
-                </address>
-        </div>
-        <div>
-          <AnimalCard />
-          <EmployeeCard />
-          <LocationCard />
-          <OwnerCard />
-            </div>
-        </div>
-        );
-    }
+      <>
+        <NavBar />
+          <ApplicationViews />
+        </>
+    );
+  }
 }
 
 export default Kennel
